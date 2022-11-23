722ab44897d95695f510fd7baf9c4c7cdaab5ac2
Alejandro Daniele <alejandro.daniele88@gmail.com>
2022-11-23 15:21:33 -0300

    Primeros pasos en JS

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//EJERCICIO A
+let numeroA = parseInt(prompt("Ingrese un número"));
+if(numeroA>1000){
+    alert("El numero es mayor a 1000");
+}
+
+//EJERCICIO B
+let palabra = prompt("Ingrese una palabra")
+if((palabra == "Hola")||(palabra == "hola")||(palabra == "HOLA")){
+    alert("El saludo es correcto")
+}
+
+//EJERCICIO C
+let numeroC = parseInt(prompt("Ingrese un número"));
+if((numeroC>10) && (numeroC<50)){
+    alert("El numero se encuentra entre 10 y 50");
+}
+
